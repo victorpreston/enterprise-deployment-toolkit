2703ad4e743896542ae6590b359f0bf4c1520869
Kyle Brennan <kyle@gitpod.io>
2024-07-16 23:24:02 +0000

    Delete instances, roles and security groups, witout having created them in the same run

--- gitpod-network-check/cmd/common.go
+++ gitpod-network-check/cmd/common.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/aws/aws-sdk-go-v2/service/iam"
 	iam_types "github.com/aws/aws-sdk-go-v2/service/iam/types"
 	log "github.com/sirupsen/logrus"
@@ -18,6 +19,7 @@ var (
 	SecurityGroups  []string
 	Roles           []string
 	InstanceProfile string
+	Subnets         map[string]bool
 )
 
 const gitpodRoleName = "GitpodNetworkCheck"
@@ -35,6 +37,24 @@ func initAwsConfig(ctx context.Context, region string) (aws.Config, error) {
 }
 
 func cleanup(ctx context.Context, svc *ec2.Client, iamsvc *iam.Client) {
+	if len(InstanceIds) == 0 {
+		instances, err := svc.DescribeInstances(ctx, &ec2.DescribeInstancesInput{
+			Filters: []types.Filter{
+				{
+					Name:   aws.String("tag:gitpod.io/network-check"),
+					Values: []string{"true"},
+				},
+			},
+		})
+		if err != nil {
+			log.WithError(err).Warn("Failed to list instances, please cleanup manually")
+		}
+
+		for _, i := range instances.Reservations[0].Instances {
+			InstanceIds = append(InstanceIds, *i.InstanceId)
+		}
+	}
+
 	if len(InstanceIds) > 0 {
 		_, err := svc.TerminateInstances(ctx, &ec2.TerminateInstancesInput{
 			InstanceIds: InstanceIds,
@@ -42,7 +62,33 @@ func cleanup(ctx context.Context, svc *ec2.Client, iamsvc *iam.Client) {
 		if err != nil {
 			log.WithError(err).WithField("instanceIds", InstanceIds).Warnf("Failed to cleanup instances, please cleanup manually")
 		}
+
+		log.Info("✅ Instances terminated")
 	}
+
+	if len(Roles) == 0 {
+		roles, err := iamsvc.ListRoles(ctx, &iam.ListRolesInput{
+			PathPrefix: aws.String("/GitpodNetworkCheck"),
+		})
+		if err != nil {
+			log.WithError(err).Warn("Failed to list roles, please cleanup manually")
+		}
+
+		for _, role := range roles.Roles {
+			if role.RoleName == nil {
+				continue
+			}
+
+			if *role.RoleName == gitpodRoleName {
+				Roles = append(Roles, *role.RoleName)
+			}
+		}
+	}
+
+	if InstanceProfile == "" {
+		InstanceProfile = gitpodInstanceProfile
+	}
+
 	if len(Roles) > 0 {
 		for _, role := range Roles {
 			_, err := iamsvc.DetachRolePolicy(ctx, &iam.DetachRolePolicyInput{PolicyArn: aws.String("arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"), RoleName: aws.String(role)})
@@ -61,7 +107,10 @@ func cleanup(ctx context.Context, svc *ec2.Client, iamsvc *iam.Client) {
 			_, err = iamsvc.DeleteRole(ctx, &iam.DeleteRoleInput{RoleName: aws.String(role)})
 			if err != nil {
 				log.WithError(err).WithField("rolename", role).Warnf("Failed to cleanup role, please cleanup manaullay")
+				continue
 			}
+
+			log.Infof("✅ Role '%v' deleted", role)
 		}
 
 		_, err := iamsvc.DeleteInstanceProfile(ctx, &iam.DeleteInstanceProfileInput{
@@ -71,10 +120,31 @@ func cleanup(ctx context.Context, svc *ec2.Client, iamsvc *iam.Client) {
 		if err != nil {
 			log.WithError(err).WithField("instanceProfile", InstanceProfile).Warnf("Failed to clean up instance profile, please cleanup manually")
 		}
+
+		log.Info("✅ Instance profile deleted")
 	}
 
 	log.Info("Cleaning up: Waiting for 1 minute so network interfaces are deleted")
 	time.Sleep(time.Minute)
+
+	if len(SecurityGroups) == 0 {
+		securityGroups, err := svc.DescribeSecurityGroups(ctx, &ec2.DescribeSecurityGroupsInput{
+			Filters: []types.Filter{
+				{
+					Name:   aws.String("tag:gitpod.io/network-check"),
+					Values: []string{"true"},
+				},
+			},
+		})
+
+		if err != nil {
+			log.WithError(err).Error("Failed to list security groups, please cleanup manually")
+		}
+
+		for _, sg := range securityGroups.SecurityGroups {
+			SecurityGroups = append(SecurityGroups, *sg.GroupId)
+		}
+	}
 
 	if len(SecurityGroups) > 0 {
 		for _, sg := range SecurityGroups {
@@ -85,9 +155,9 @@ func cleanup(ctx context.Context, svc *ec2.Client, iamsvc *iam.Client) {
 			_, err := svc.DeleteSecurityGroup(ctx, deleteSGInput)
 			if err != nil {
 				log.WithError(err).WithField("securityGroup", sg).Warnf("Failed to clean up security group, please cleanup manually")
+				continue
 			}
-
+			log.Infof("✅ Security group '%v' deleted", sg)
 		}
-
 	}
 }
