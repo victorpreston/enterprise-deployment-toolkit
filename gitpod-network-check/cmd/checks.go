55ecf063649ead0eaeafc83a4e14b8a84f816dcf
Kyle Brennan <kyle@gitpod.io>
2025-03-06 21:19:36 +0000

    Spacing

--- gitpod-network-check/cmd/checks.go
+++ gitpod-network-check/cmd/checks.go
@@ -91,7 +91,7 @@ var checkCommand = &cobra.Command{ // nolint:gochecknoglobals
 		if err != nil {
 			return fmt.Errorf("❌ Nodes never got Running: %v", err)
 		}
-		log.Info("✅  EC2 instances are now Running.")
+		log.Info("✅ EC2 instances are now Running.")
 		log.Info("ℹ️  Waiting for EC2 instances to become Healthy (times out in 5 minutes)")
 		waitstatusOK := ec2.NewInstanceStatusOkWaiter(ec2Client, func(isow *ec2.InstanceStatusOkWaiterOptions) {
 			isow.MaxDelay = 15 * time.Second
